d8189dbd3035752dd7afca5e9c306b70f12d4774
Iván Cuevas <irving_ivan1906@hotmail.com>
2022-05-04 16:04:36 -0500

    modified file

--- main.py
+++ main.py
@@ -1,146 +1,208 @@
 from turtle import Screen
 import pygame, sys
 from pygame.locals import *
+import random
 
-anchoPantalla = 400
+pygame.init()
+
+### Elemento Clock para controlar el movimiento
+clock = pygame.time.Clock()
+fps = 30
+
+anchoPantalla = 500
 altoPantalla = 650
-velocidad = 10
-gravedad = 1
-velocidad_Juego = 10
-espacioTubo = 150
 
+pantalla = pygame.display.set_mode((anchoPantalla, altoPantalla))
+pygame.display.set_caption("Flappy Mario")
+
+pygame.mixer.music.load("assets/principal.mp3")
+pygame.mixer.music.play()
+pygame.mixer.music.set_volume(0.2)
+
+#Cargando música y sonidos
+sonidoSalto = pygame.mixer.Sound("assets/salto.mp3")
+
+#Definiendo fuente
+fuente = pygame.font.SysFont('Bauhaus 93', 60)
+
+#Definiendo color
+blanco = (255, 255, 255)
+
+
+#Definiendo variables del juego
+velocidad = 8
+gravedad = 1
+desplazamientoPiso = 0
+velocidadJuego = 6
+gameOver = False
+vuelo = False
+espacioTubo = 150
+frecuenciaTubo = 1500 #Milisegundos
+ultimoTubo = pygame.time.get_ticks() - frecuenciaTubo
 ancho_piso = 2 * anchoPantalla
 altura_piso = 100
+puntaje = 0
+pasarTuberia = False
+
+#Cargando imágenes
+fondo = pygame.image.load("assets/fondo.png")
+fondo = pygame.transform.scale(fondo, (anchoPantalla, altoPantalla))
+piso = pygame.image.load("assets/piso1.png")
+
+
+def textoPuntuacion(texto, fuente, colorTexto, x, y):
+    img = fuente.render(texto, True, colorTexto)
+    pantalla.blit(img, (x, y))
 
 #Clase para crear a Mario
 class Mario(pygame.sprite.Sprite):
 
     #Función para crear un sprite de 3 Marios
-    def __init__(self):
+    def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
 
-        self.images = [pygame.image.load("assets/mario1.png").convert_alpha(),
-                        pygame.image.load("assets/mario2.png").convert_alpha(),
-                        pygame.image.load("assets/mario3.png").convert_alpha()]
+        self.images = []
+        self.index = 0
+        self.counter = 0
+        for num in range(1, 4):
+            img = pygame.image.load(f'assets/mario{num}.png')
+            self.images.append(img)
+        self.image = self.images[self.index]
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+        self.vel = 0
+        self.clicked = False
 
         self.speed = velocidad
 
-        self.current_image = 0
+        
 
-        self.image = pygame.image.load("assets/mario1.png").convert_alpha()
-        self.mask = pygame.mask.from_surface(self.image)
-
-        self.rect = self.image.get_rect()
-        self.rect[0] = anchoPantalla / 2
-        self.rect[1] = altoPantalla / 2
-
-    #Función para controlar el movimiento de Mario (VER VIDEO PARA CORROBORAR)
+    #Función para controlar el movimiento de Mario
     def update(self):
-        self.current_image = (self.current_image + 1) % 3
-        self.image = self.images[ self.current_image ]
-
-
         self.speed += gravedad
-
+        
         # Actualizar altura
-        self.rect[1] += self.speed
+        if vuelo == True:
+            self.rect[1] += self.speed
+            
+            
+
+        if gameOver == False:
+
+            #Manejo de la animación
+            self.counter += 1
+            cuentaColeo = 5
+
+            if self.counter > cuentaColeo:
+                self.counter = 0
+                self.index += 1
+                if self.index >= len(self.images):
+                    self.index = 0
+            self.image = self.images[self.index]
 
     #Función para controlar la velocidad del cambio entre un sprite y otro
-    def bump(self):
+    def jump(self):
         self.speed = -velocidad
+        
 
 #Clase para crear el tubo
 class Tubo(pygame.sprite.Sprite):
 
     def __init__(self, x, y, posicion) -> None:
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("assets/tuboinferior.png")
+        self.image = pygame.image.load("assets/tubo.png")
         self.rect = self.image.get_rect()
         #Posición 1 es para el tubo superior, y el -1 para tubo inferior
         if posicion == 1:
             self.image = pygame.transform.flip(self.image, False, True)
-            self.rect.bottomleft = [x, y - (espacioTubo / 2)]
+            self.rect.bottomleft = [x, y - int(espacioTubo / 2)]
         if posicion == -1:
-            self.rect.topleft = [x, y + (espacioTubo / 2)]
+            self.rect.topleft = [x, y + int(espacioTubo / 2)]
     
-
-#Clase para crear el piso
-class Piso(pygame.sprite.Sprite):
-
-    def __init__(self, xpos):
-        pygame.sprite.Sprite.__init__(self)
-
-        self.image = pygame.image.load("assets/piso.png").convert_alpha()
-        self.image = pygame.transform.scale(self.image, (ancho_piso, altura_piso))
-        
-        self.mask = pygame.mask.from_surface(self.image)
-
-        self.rect = self.image.get_rect()
-        self.rect[0] = xpos
-        self.rect[1] = altoPantalla - altura_piso
-    
+    #Movimiento de la tubería
     def update(self):
-        self.rect[0] -= velocidad_Juego
-
-def is_off_screen(sprite):
-    return sprite.rect[0] < -(sprite.rect[2])
-
-pygame.init()
-pantalla = pygame.display.set_mode((anchoPantalla, altoPantalla))
-pygame.display.set_caption("Flappy Mario")
-
-fondo = pygame.image.load("assets/fondo.png")
-fondo = pygame.transform.scale(fondo, (anchoPantalla, altoPantalla))
+        self.rect.x -= velocidadJuego
+        if self.rect.right < 0:
+            self.kill()
 
 
 grupo_mario = pygame.sprite.Group()
-mario = Mario()
+grupo_tubo = pygame.sprite.Group()
+
+mario = Mario(100, int(altoPantalla / 2))
+
 grupo_mario.add(mario)
 
-grupo_tubo = pygame.sprite.Group()
-tubo_inferior = Tubo(300, (altoPantalla/2), -1)
-tubo_superior = Tubo(300, (altoPantalla/2), 1)
-grupo_tubo.add(tubo_inferior)
-grupo_tubo.add(tubo_superior)
+posicionPiso = posicionPiso = altoPantalla - piso.get_height()
 
-grupo_piso = pygame.sprite.Group()
-for i in range(2):
-    piso = Piso(ancho_piso * i)
-    grupo_piso.add(piso)
+run = True
+while run:
+    clock.tick(fps)
 
-### Elemento Clock para controlar el movimiento de Mario.
-clock = pygame.time.Clock()
-
-while True:
-    clock.tick(20)
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-        
-        if event.type == KEYDOWN:
-            if event.key == K_SPACE:
-                mario.bump()
-
+    #Dibujar fondo
     pantalla.blit(fondo, (0,0))
 
-    # Ciclo IF para crear piso "infinito"
-    if is_off_screen(grupo_piso.sprites()[0]):
-        grupo_piso.remove(grupo_piso.sprites()[0])
-        
-        nuevo_piso = Piso(ancho_piso - 10)
-        grupo_piso.add(nuevo_piso)
-
-    grupo_tubo.update()
-    grupo_mario.update()
-    grupo_piso.update()
-
-    grupo_tubo.draw(pantalla)
     grupo_mario.draw(pantalla)
-    grupo_piso.draw(pantalla)
+    grupo_mario.update()
+    grupo_tubo.draw(pantalla)
 
-    if pygame.sprite.groupcollide(grupo_mario, grupo_piso, False, False, pygame.sprite.collide_mask):
-        #Game Over
-        input()
-        break
     
+
+    #Dibujar piso
+    pantalla.blit(piso, (desplazamientoPiso, posicionPiso))
+
+    #Puntaje
+    if len(grupo_tubo) > 0:
+        if grupo_mario.sprites()[0].rect.left > grupo_tubo.sprites()[0].rect.left\
+            and grupo_mario.sprites()[0].rect.right < grupo_tubo.sprites()[0].rect.right\
+            and pasarTuberia == False:
+            pasarTuberia = True
+        if pasarTuberia == True:
+            if grupo_mario.sprites()[0].rect.left > grupo_tubo.sprites()[0].rect.right:
+                puntaje += 1
+                pasarTuberia = False
+
+    textoPuntuacion(str(puntaje), fuente, blanco, int(anchoPantalla / 2), 20)
+
+    #Busca de colisión
+    if pygame.sprite.groupcollide(grupo_mario, grupo_tubo, False, False) or mario.rect.top < 0:
+        gameOver = True
+        
+    #Chequeo si Mario tiene un tope en el suelo
+    if mario.rect.bottom > posicionPiso:
+        gameOver = True
+        vuelo = False
+
+    if gameOver == False and vuelo == True:
+
+        #Generación de nuevas tuberías
+        ahora = pygame.time.get_ticks()
+        if ahora - ultimoTubo > frecuenciaTubo:
+            alturaTubo = random.randint(-100, 100)
+            tubo_inferior = Tubo(anchoPantalla, (altoPantalla/2) + alturaTubo, -1)
+            tubo_superior = Tubo(anchoPantalla, (altoPantalla/2) + alturaTubo, 1)
+            grupo_tubo.add(tubo_inferior)
+            grupo_tubo.add(tubo_superior)
+            ultimoTubo = ahora
+
+        #Dibuja y desplaza el piso
+        desplazamientoPiso -= velocidadJuego
+        if abs(desplazamientoPiso) > 35:
+            desplazamientoPiso = 0
+        
+        grupo_tubo.update()    
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == KEYDOWN and vuelo == False and gameOver == False:
+            if event.key == K_SPACE:
+                vuelo = True         
+               
+        mario.jump()
+        sonidoSalto.play()
+        sonidoSalto.set_volume(0.1)         
+
     pygame.display.update()
+
+pygame.quit()
