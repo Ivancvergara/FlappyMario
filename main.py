db816dbd189ee2383d374dec5b6b74ad75392dbd
Iván Cuevas <irving_ivan1906@hotmail.com>
2022-05-09 22:03:56 -0500

    modified file

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from turtle import Screen
-import pygame, sys
+import pygame
+from pathlib import Path
 from pygame.locals import *
 import random
+
 
 pygame.init()
 
@@ -15,19 +16,19 @@ altoPantalla = 650
 pantalla = pygame.display.set_mode((anchoPantalla, altoPantalla))
 pygame.display.set_caption("Flappy Mario")
 
-pygame.mixer.music.load("assets/principal.mp3")
-pygame.mixer.music.play()
-pygame.mixer.music.set_volume(0.2)
-
 #Cargando música y sonidos
-sonidoSalto = pygame.mixer.Sound("assets/salto.mp3")
+
+musicaFondo = pygame.mixer.Sound("assets/principal.mp3")
+sonidoSalto = pygame.mixer.Sound("assets/salto.wav")
+sonidoMuerte = pygame.mixer.Sound("assets/muerto.wav")
+
 
 #Definiendo fuente
-fuente = pygame.font.SysFont('Bauhaus 93', 60)
+fuenteGameOver = pygame.font.SysFont('Bauhaus 93', 25)
+fuentePuntaje = pygame.font.SysFont('Bauhaus 93', 60)
 
 #Definiendo color
 blanco = (255, 255, 255)
-
 
 #Definiendo variables del juego
 velocidad = 8
@@ -44,15 +45,48 @@ altura_piso = 100
 puntaje = 0
 pasarTuberia = False
 
+#Manejo de Archivo Mejor Puntaje
+current_path = Path.cwd()
+file_Path = current_path / "mejorPuntaje.txt"
+
 #Cargando imágenes
 fondo = pygame.image.load("assets/fondo.png")
 fondo = pygame.transform.scale(fondo, (anchoPantalla, altoPantalla))
 piso = pygame.image.load("assets/piso1.png")
+img_GameOver = pygame.image.load("assets/gameover.png")
 
 
-def textoPuntuacion(texto, fuente, colorTexto, x, y):
-    img = fuente.render(texto, True, colorTexto)
+def textoFelicitacion(texto, fuenteGameOver, colorTexto, x, y):
+    img = fuenteGameOver.render(texto, True, colorTexto)
     pantalla.blit(img, (x, y))
+
+def textoMejorPuntaje(texto, fuenteGameOver, colorTexto, x, y):
+    img = fuenteGameOver.render(texto, True, colorTexto)
+    pantalla.blit(img, (x, y))
+
+def textoPuntaje(texto, fuentePuntaje, colorTexto, x, y):
+    img = fuentePuntaje.render(texto, True, colorTexto)
+    pantalla.blit(img, (x, y))
+
+def escrituraPuntaje():
+    #Manejo de Archivo (Escritura, sobreescribiendo todo)
+    with open(file_Path, "w") as file:
+        file.write(str(puntaje))
+
+class lecturaPuntaje:
+    #Manejo de Archivo (Sólo Lectura)
+    with open(file_Path) as file:
+        content = file.read()
+
+def reset_game():
+    grupo_tubo.empty()
+    mario.rect.x = 100
+    mario.rect.y = int(altoPantalla / 2)
+    puntaje = 0
+    return puntaje
+
+musicaFondo.set_volume(0.2)
+musicaFondo.play()
 
 #Clase para crear a Mario
 class Mario(pygame.sprite.Sprite):
@@ -60,7 +94,6 @@ class Mario(pygame.sprite.Sprite):
     #Función para crear un sprite de 3 Marios
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-
         self.images = []
         self.index = 0
         self.counter = 0
@@ -76,16 +109,14 @@ class Mario(pygame.sprite.Sprite):
         self.speed = velocidad
 
         
-
     #Función para controlar el movimiento de Mario
     def update(self):
         self.speed += gravedad
-        
+
+
         # Actualizar altura
         if vuelo == True:
             self.rect[1] += self.speed
-            
-            
 
         if gameOver == False:
 
@@ -108,7 +139,7 @@ class Mario(pygame.sprite.Sprite):
 #Clase para crear el tubo
 class Tubo(pygame.sprite.Sprite):
 
-    def __init__(self, x, y, posicion) -> None:
+    def __init__(self, x, y, posicion):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("assets/tubo.png")
         self.rect = self.image.get_rect()
@@ -125,6 +156,27 @@ class Tubo(pygame.sprite.Sprite):
         if self.rect.right < 0:
             self.kill()
 
+class juegoTerminado():
+    def __init__(self, x, y, image):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+
+    def draw(self):
+        action = False
+
+        #get mouse position
+        pos = pygame.mouse.get_pos()
+
+        #check if mouse is over the button
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1:
+                action = True
+
+        #draw button
+        pantalla.blit(self.image, (self.rect.x, self.rect.y))
+        return action
+
 
 grupo_mario = pygame.sprite.Group()
 grupo_tubo = pygame.sprite.Group()
@@ -134,6 +186,8 @@ mario = Mario(100, int(altoPantalla / 2))
 grupo_mario.add(mario)
 
 posicionPiso = posicionPiso = altoPantalla - piso.get_height()
+
+juegoTerminado = juegoTerminado((anchoPantalla/2) - 96, (altoPantalla/2) - 30, img_GameOver)
 
 run = True
 while run:
@@ -145,8 +199,6 @@ while run:
     grupo_mario.draw(pantalla)
     grupo_mario.update()
     grupo_tubo.draw(pantalla)
-
-    
 
     #Dibujar piso
     pantalla.blit(piso, (desplazamientoPiso, posicionPiso))
@@ -162,8 +214,8 @@ while run:
                 puntaje += 1
                 pasarTuberia = False
 
-    textoPuntuacion(str(puntaje), fuente, blanco, int(anchoPantalla / 2), 20)
-
+    textoPuntaje(str(puntaje), fuentePuntaje, blanco, int(anchoPantalla / 2), 20)
+    
     #Busca de colisión
     if pygame.sprite.groupcollide(grupo_mario, grupo_tubo, False, False) or mario.rect.top < 0:
         gameOver = True
@@ -190,7 +242,23 @@ while run:
         if abs(desplazamientoPiso) > 35:
             desplazamientoPiso = 0
         
-        grupo_tubo.update()    
+        grupo_tubo.update()
+
+    #Checar GameOver y reiniciar
+    if gameOver == True:
+        if puntaje > int(lecturaPuntaje.content):
+            escrituraPuntaje()
+            textoFelicitacion("¡Felicidades has superado el Mejor Puntaje!", fuenteGameOver, blanco, 5, int((altoPantalla / 5) * 3))
+            textoMejorPuntaje("El Mejor Puntaje era de: " + lecturaPuntaje.content, fuenteGameOver, blanco, int((anchoPantalla / 2) - 120), int((altoPantalla / 5) * 3.5))
+            textoPuntaje("Tu Puntaje fue de: " + str(puntaje), fuenteGameOver, blanco, int((anchoPantalla / 2) - 100), int((altoPantalla / 5) * 4))
+        else:
+            textoFelicitacion("No has superado el Mejor Puntaje :(", fuenteGameOver, blanco, int(anchoPantalla / 8), int((altoPantalla / 5) * 3))
+            textoMejorPuntaje("El Mejor Puntaje es: " + lecturaPuntaje.content, fuenteGameOver, blanco, int((anchoPantalla / 2) - 100), int((altoPantalla / 5) * 3.5))
+            textoPuntaje("Tu Puntaje fue de: " + str(puntaje), fuenteGameOver, blanco, int((anchoPantalla / 2) - 100), int((altoPantalla / 5) * 4))
+        if juegoTerminado.draw() == True:
+            gameOver = False
+            puntaje = reset_game()
+            
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
